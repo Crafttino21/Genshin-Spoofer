7af52e3a09595d80f6e3958c53f4d1c74650ff02
WeepingAngel <58913870+Crafttino21@users.noreply.github.com>
2024-07-14 13:16:15 +0200

    Updated to 1.1
    Bug fixes and Functions

--- genSpoof.py
+++ genSpoof.py
@@ -16,7 +16,7 @@ banner = '''
 ╚██████╔╝███████╗██║ ╚████║███████║██║  ██║██║██║ ╚████║    ███████║██║     ╚██████╔╝╚██████╔╝██║     
  ╚═════╝ ╚══════╝╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝    ╚══════╝╚═╝      ╚═════╝  ╚═════╝ ╚═╝     
         Genshin Version Spoofer by WeepingAngel | GitHub: https://github.com/Crafttino21                                                                                      
-                    Version: 1.0  | Build: 1 (GitHub) | Windows Only
+                    Version: 1.1  | Build: 1 (GitHub) | Windows Only
 '''
 
 warning_text = '''
@@ -53,6 +53,8 @@ Choose your version you want to spoof!
 [3] Version 4.6.0
 [4] Version 4.5.0
 [5] Custom Version
+=======================================
+[6] Supress/Disable Autoupdates (BETA)
 '''
 
 
@@ -97,6 +99,26 @@ class modules:
         else:
             print(colors.red + "Section 'General' or key 'game_version' not found.")
 
+    @staticmethod
+    def supress_update_patch(file_path):
+        config = configparser.ConfigParser()
+        if not os.path.exists(file_path):
+            print(colors.red + f"File {file_path} not found!")
+            return
+
+        config.read(file_path)
+
+        if 'General' in config and 'channel' in config['General']:
+            config['General']['channel'] = '0'  # Wert als String setzen
+
+            with open(file_path, 'w') as configfile:
+                config.write(configfile)
+            print(colors.magenta + "Patching Game...")
+            time.sleep(3)
+            print(colors.green + "Update Supress Fix Patched!")
+        else:
+            print(colors.red + "Section 'General' or key 'channel' not found.")
+
 
 
 
@@ -104,12 +126,24 @@ class version_build:
     @staticmethod
     def spoof_version(version):
         main_installation_directory = input(
-            "Please enter the main installation directory (the 'Genshin Impact' folder): ")
+            f"{colors.magenta}Please enter the main installation directory (the 'Genshin Impact' folder): ")
         time.sleep(3)
         config_file_path = os.path.join(main_installation_directory, 'Genshin Impact game', 'config.ini')
         modules.update_game_version(config_file_path, version)
         print(colors.green + succ)
         syscalls.Mbox('Genshin Spoofer by WeepingAngel', f'Spoofed successfully to {version}', 64)
+        exit()
+
+    @staticmethod
+    def supress_update():
+        syscalls.Mbox('Genshin Spoofer by WeepingAngel', 'ATTENTION! This is a testing Feature that might be not work!', 48)
+        main_installation_directory = input(
+            "Please enter the main installation directory (the 'Genshin Impact' folder): ")
+        time.sleep(3)
+        config_file_path = os.path.join(main_installation_directory, 'Genshin Impact game', 'config.ini')
+        modules.supress_update_patch(config_file_path)
+        print(colors.green + succ)
+        syscalls.Mbox('Genshin Spoofer by WeepingAngel', f'Patched Update Supression Succsessfully!', 64)
         exit()
 
 
@@ -123,13 +157,13 @@ class warning:
             return True
         else:
             print(colors.red + "You didn't accept the Terms of Service!")
-            syscalls.Mbox('Genshin Spoofer by WeepingAngel', 'You Dont aceept the Terms of Service!', 16)
+            syscalls.Mbox('Genshin Spoofer by WeepingAngel', 'You Dont accept the Terms of Service!', 16)
             exit()
 
 
 print(colors.magenta + banner)
 syscalls.changeTitle('Genshin Spoofer by WeepingAngel')
-syscalls.Mbox('Genshin Spoofer by WeepingAngel', 'Thanks for Download! | Version: 1.0', 64)
+syscalls.Mbox('Genshin Spoofer by WeepingAngel', 'Thanks for Download! | Version: 1.1', 64)
 if not warning.warning_msg():
     exit()
 syscalls.Mbox('Genshin Spoofer by WeepingAngel', 'Thanks for Accepting the Terms of Service!', 64)
@@ -151,5 +185,9 @@ elif ver == "5":
     custom_version = input("Enter value (only numbers, format: x.x.x) > ")
     syscalls.changeTitle(f'Spoofing to {custom_version} Version')
     version_build.spoof_version(custom_version)
+elif ver == "6":
+    syscalls.clearConsole()
+    syscalls.changeTitle("Disable Updates Genshin Spoofer by WeepingAngel")
+    version_build.supress_update()
 else:
     print(colors.red + "Invalid selection. Please try again.")
